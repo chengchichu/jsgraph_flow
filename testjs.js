96c2b2506937ad27b0b6c1023d461088e5fe6844
chengchichu <glacrimosa@gmail.com>
2023-01-11 16:32:31 +0800

    load json and parse to dot

--- testjs.js
+++ testjs.js
@@ -77,6 +77,8 @@
 // I'll choose map over obj for its preservation of datatype of key
 
 
+
+
 var dotSrc = `
 
 digraph {
@@ -88,6 +90,9 @@ digraph {
 }    
 `
 
+
+
+'{"directed": true, "multigraph": false, "graph": {}, "nodes": [{"id": 0}, {"id": 1}, {"id": 2}, {"id": 3}, {"id": 4}, {"id": 5}, {"id": 6}, {"id": 7}, {"id": 8}, {"id": 9}], "links": [{"source": 1, "target": 0}, {"source": 2, "target": 0}, {"source": 3, "target": 1}, {"source": 4, "target": 1}, {"source": 5, "target": 2}, {"source": 6, "target": 2}, {"source": 7, "target": 3}, {"source": 8, "target": 7}, {"source": 9, "target": 1}]}'
 
 // nested obj constructor
 
@@ -120,6 +125,8 @@ var g = {
    }
    }  // edge
 };
+
+
 
 
 String.format = function() {
@@ -163,58 +170,93 @@ function parse_graph(g){
 console.log(parse_graph(g))
 
 
-// const default_property = new Map();
-// default_property.set('shape',"box")
-// default_property.set('style',"rounded")
-// default_property.set('fontname',"Lato")
-// default_property.set('color',"black")
-// default_property.set('margin',"0.2")
+// animation
 
-// for (const [key, value] of default_property.entries()) {
-//    console.log(key + ": " + value)
-//    }
+// d3.select("#graph").graphviz().renderDot('digraph {a -> b}')
 
+// nodes = d3.selectAll('.node')
+// var graphviz = d3.select("#graph").graphviz();
 
-// var g = new Object();  
-// g.default_shape=default_property;  
+// function render() {
+//    console.log('DOT source =', dotSrc);
+//    dotSrcLines = dotSrc.split('\n');
 
-// for (const key of Object.keys(g)) {
-//    console.log(g[key])
-//    };
-
-// g.default_style="rounded";  
-// g.default_fontname="Lato";  
-// g.default_color="black";  
-// g.default_margin="0.2";  
-// g
-
-// function g(default_propperty,node,edge){  
-//    this.default_shape=default_propperty[0];  
-//    this.name=name;  
-//    this.salary=salary;  
-//    }  
-//    e=new emp(103,"Vimal Jaiswal",30000); 
-
-
-// var dotSrc = `
-
-// digraph {
-//     node [ shape="box", style="rounded", fontname="Lato", margin=0.2, color="black"]
-//     feature1 [ label="feature1" id="f1" fontcolor="blue" color="blue" ]
-//     swip1 [ label="swip1" id="sp1" fontcolor="#000000" color="blue" ]
-    
-//     feature1 -> swip1 [label="f1_to_sp1" id="E_f1sp1"]
-// }    
-// `
-
+//    graphviz
+//        .transition(function() {
+//            return d3.transition()
+//                .delay(100)
+//                .duration(1000);
+//        })
+//        .renderDot(dotSrc)
+//        .on("end", interactive);
+// }
 
 d3.select("#graph").graphviz().renderDot(dotSrc)
-   .on("end", interactive)
+     .on("end", interactive)
+
 
 function interactive() {   
    //  nodes = d3.selectAll('.node,.edge')
    nodes = d3.selectAll('.node')
-   console.log(nodes)
+   // console.log(nodes)
+   // console.log(d3.select("svg").datum())
+   // console.
+   // console.log(JSON.stringify(d3.select("svg").datum(), null, 4));
+
+   nodes
+       .on("click", function () {
+            var title = d3.select(this).selectAll('title').text().trim();
+            var text = d3.select(this).selectAll('text').text();
+            var id = d3.select(this).attr("id","hello");
+            var class1 = d3.select(this).attr('class');
+            var sobj = d3.select(this)
+            console.log(id)
+            // console.log(title)
+            // var astr = ` swip5 [ label="swip5" id="node_sp5" fontcolor="#000000" color="black" ]`
+            
+            // var newstr = astr.split(" color")[0]+` color="yellow" ]`
+            // console.log(newstr)
+            
+             
+   });
+};   
+
+// render(dotSrc);
+// const fs = require("fs");
+// fs.readFile(filePath, function (error, content) {
+//    var data = JSON.parse(content);
+//    console.log(data.collection.length);
+// });
+// filePath = "/Users/chengchichu/Desktop/py/jsgraph_flow/sample.json"
+
+// import data from '/Users/chengchichu/Desktop/py/jsgraph_flow/sample.json'
+
+// console.log(data)
+
+function readTextFile(file, callback) {
+   var rawFile = new XMLHttpRequest();
+   rawFile.overrideMimeType("application/json");
+   rawFile.open("GET", file, true);
+   rawFile.onreadystatechange = function() {
+       if (rawFile.readyState === 4 && rawFile.status == "200") {
+           callback(rawFile.responseText);
+       }
+   }
+   rawFile.send(null);
 }
 
+//usage:
+readTextFile("./sample.json", function(text){
+   var data = JSON.parse(text);
+   console.log(data);
+
+   for (const key of Object.keys(data.nodes)) {     
+       console.log(data.nodes[key])
+       
+       var node_i = String.format(`    "{0}" [ label="{1}", id="{2}", frontcolor="{3}", color="{4}" ]`,key,g.node[key].label,g.node[key].id,g.node[key].fontcolor,g.node[key].color)
+     // paresed_node.push(node_i)
+  };
+
+
+});
 
